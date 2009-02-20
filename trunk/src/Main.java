03b80ed258ff46bf0f75ca273158cc2696ffb1e4
simon.kotlinski <simon.kotlinski@0e5cefce-fe30-11dd-984d-ab748d24aa7e>
2009-02-20 20:25:20 +0000

    Har lagt till en metod som heter moveStartDate(task a) i main. Den är inte genomtänkt. men vi kan kanske flytta den och använda koden.

--- trunk/src/Main.java
+++ trunk/src/Main.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * 
  */
@@ -9,13 +11,35 @@
  *
  */
 public class Main {
-
+	ArrayList<Task> tasks = new ArrayList<Task>();
+	
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+	}
+	
+	/**
+	 * moveMent - Om en en task crashar med en annan efter att
+	 * man har flyttat den.
+	 * 
+	 * @return true om flytten är ok!
+	 * @param args
+	 */
+	public boolean moveStartDate(Task a) {
+		for (int i=0; i<tasks.size();i++){
+			if (a.getId()!=tasks.get(i).getId()){
+				//om task a's start datum ligger i någon annan tasks-intervall... då returnerar vi false.
+				if (a.getStartDate().getTimeInMillis()<tasks.get(i).getEndDate().getTimeInMillis() && a.getStartDate().getTimeInMillis()>tasks.get(i).getStartDate().getTimeInMillis()){
+					return false;
+					//Du har försökt ändra Task a's start-datum?
+				}
+			}
+		}
+		
+		return true;
 	}
 
 }
