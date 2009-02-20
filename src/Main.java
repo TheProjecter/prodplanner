a155afaf2f941b150f8640838fa9e48c463ecd3e
simon.kotlinski <simon.kotlinski@0e5cefce-fe30-11dd-984d-ab748d24aa7e>
2009-02-20 21:38:49 +0000

    Ändrat Millis() en funktion....

--- src/Main.java
+++ src/Main.java
@@ -36,9 +36,31 @@ public class Main {
 					return false;
 					//Du har försökt ändra Task a's start-datum?
 				}
+				else {
+					
+				}
 			}
 		}
-		
+		return true;
+	}
+	
+	/**
+	 * moveMent - Om en en task crashar med en annan efter att
+	 * man har ändrat/flyttat dess duration.
+	 * 
+	 * @return true om flytten är ok!
+	 * @param args
+	 */
+	public boolean moveEndDate(Task a) {
+		for (int i=0; i<tasks.size();i++){
+			if (a.getId()!=tasks.get(i).getId()){
+				//om task a's start datum ligger i någon annan tasks-intervall... då returnerar vi false.
+				if (a.getStartDateInMillis()<tasks.get(i).getEndDateInMillis() && a.getStartDate().getTimeInMillis()>tasks.get(i).getStartDateInMillis()){
+					return false;
+					//Du har försökt ändra Task a's start-datum?
+				}
+			}
+		}
 		return true;
 	}
 
