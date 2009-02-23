34f53e7d6791ec6181f79cc9e4c6a08229fe7dc2
simon.kotlinski <simon.kotlinski@0e5cefce-fe30-11dd-984d-ab748d24aa7e>
2009-02-23 12:46:58 +0000

    Har Lagt till så att man kan skapa nya rader i jTable nu!

--- src/Views/ProdPlannerUI.java
+++ src/Views/ProdPlannerUI.java
@@ -5,6 +5,9 @@ import java.awt.Dimension;
 import javax.swing.JButton;
 import java.awt.Rectangle;
 import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+
+import Controllers.TaskController;
 
 public class ProdPlannerUI {
 
@@ -12,7 +15,9 @@ public class ProdPlannerUI {
 	private JPanel jContentPane = null;
 	private JButton jButton = null;
 	private JTable jTable = null;
-	
+	TaskController taskController = new TaskController();  //  @jve:decl-index=0:
+    DefaultTableModel model = new DefaultTableModel();
+
 
 	public ProdPlannerUI(String title)
 	{
@@ -60,6 +65,17 @@ public class ProdPlannerUI {
 			jButton = new JButton();
 			jButton.setBounds(new Rectangle(30, 232, 106, 31));
 			jButton.setText("New Task");
+			jButton.addActionListener(new java.awt.event.ActionListener() {
+				public void actionPerformed(java.awt.event.ActionEvent e) {
+					taskController.addTask();
+					System.out.println("actionPerformed()"); // TODO Auto-generated Event stub actionPerformed()
+					System.out.println(jTable.getRowCount());
+				    model.insertRow(0, new Object[]{""});
+					
+					
+					
+				}
+			});
 		}
 		return jButton;
 	}
@@ -71,8 +87,15 @@ public class ProdPlannerUI {
 	 */
 	private JTable getJTable() {
 		if (jTable == null) {
-			jTable = new JTable();
+			jTable = new JTable(model);
 			jTable.setBounds(new Rectangle(300, 197, 375, 80));
+			model.addColumn("Costumer");
+			model.addColumn("Duration");
+			model.addColumn("Start");
+			model.addColumn("End");
+			model.addColumn("Earliest");
+			model.addColumn("Latest");
+			
 			
 		}
 		return jTable;
