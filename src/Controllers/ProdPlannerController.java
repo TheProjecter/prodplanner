b56f4f0b8d11f88640346824b8dfe332ddd62d54
pompa <pompa@kth.se>
2009-02-26 12:18:25 +0000

    Fixat lite MVC stöd. Försöker flytta ut listeners till Controllers.

--- src/Controllers/ProdPlannerController.java
+++ src/Controllers/ProdPlannerController.java
@@ -1,5 +1,24 @@
 package Controllers;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+
+import Views.ProdPlannerUI;
+
 public class ProdPlannerController {
 
+	private ProdPlannerUI view;
+		
+	public ProdPlannerController(ProdPlannerUI view)
+	{
+		this.view = view;
+		
+	}
+	
+	public void windowClosing(WindowEvent e) {
+        System.exit(0);
+    }
 }
