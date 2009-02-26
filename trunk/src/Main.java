66596aa149d3bedc839da2cb17f475f62adb0c6a
pompa@kth.se <pompa@kth.se@0e5cefce-fe30-11dd-984d-ab748d24aa7e>
2009-02-26 11:48:24 +0000

    test

--- trunk/src/Main.java
+++ trunk/src/Main.java
@@ -21,6 +21,6 @@ public class Main {
 	public static void main(String[] args) {
 		// create view for main GUI
 		String title = TITLE + " " + VERSION;
-		new ProdPlannerUI(title);
+		new ProdPlannerUI(title); // najs
 	}
 }
