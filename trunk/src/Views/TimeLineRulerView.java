9ce99503c4e1f1169840149f75eca558826a0895
simon.kotlinski <simon.kotlinski@0e5cefce-fe30-11dd-984d-ab748d24aa7e>
2009-03-02 18:34:00 +0000

    Nu kan man dra i sidorna på varje låda!

--- trunk/src/Views/TimeLineRulerView.java
+++ trunk/src/Views/TimeLineRulerView.java
@@ -2,8 +2,9 @@ package Views;
 
 import javax.swing.*;
 import java.awt.*;
-
-
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
 
 public class TimeLineRulerView extends JPanel
 {
@@ -15,20 +16,23 @@ public class TimeLineRulerView extends JPanel
 		this.text = text;
 		setBackground(new Color(255,255,204));
 	}
-	
+		
 	@Override public void paintComponent(Graphics g) {
 		super.paintComponent(g);
-		
+
 		g.drawLine(0, 30, getWidth(), 30);
 		
 		for (int i = 0; i < getWidth(); i++) {
 			if (i%30 == 0) {
-				g.drawLine(i, 30, i, 25);
-				
+				if(i%7 == 0){ // varje ny vecka(måndag) ska börja med en lite högre linje
+					g.drawLine(i, 30, i, 15);
+				}
+				else{
+					g.drawLine(i, 30, i, 25);
+				}
 				
 			}
 			
 		}
 	}
-
 }
