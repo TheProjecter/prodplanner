77072014f7693d0d6ce84c7d4ee893a4a55bc8b9
simon.kotlinski <simon.kotlinski@0e5cefce-fe30-11dd-984d-ab748d24aa7e>
2009-03-03 18:41:45 +0000

    Nu snackar vi Magi!

--- trunk/src/Views/TimeLineView.java
+++ trunk/src/Views/TimeLineView.java
@@ -20,6 +20,7 @@ public class TimeLineView extends JPanel
     double s2=0;// typ2 vid grab
     int s3=0;
     int k=0; //bit som håller koll på vilket håll en box går åt.
+    int k2=-1; //vilket objekt som dras
     private int x, y; // mousepos in pane
 
 
@@ -78,12 +79,12 @@ public class TimeLineView extends JPanel
 	}
 	
 	private boolean mouseOverBox(int x, int y, int w, int h) {
-		if ( ( (this.x < x+w)&&(this.x > x)) && ((this.y > y) && (this.y < y+h))) {
-			//System.out.println("X = " + x + " Y = " + y + " w = " + w + " h = " + h);
-			  
-			return true;
-		}
-		
+//		if ( ( (this.x < x+w)&&(this.x > x)) && ((this.y > y) && (this.y < y+h))) {
+//			//System.out.println("X = " + x + " Y = " + y + " w = " + w + " h = " + h);
+//			  
+//			return true;
+//		}
+//		
 		return false;
 		
 	}
@@ -138,6 +139,7 @@ public class TimeLineView extends JPanel
        	  		for (int i =0; i<rects.size();i++){
        	  			if(rects.get(i)!=null){
        	  				if (rects.get(i).getRect().intersects(r)) {
+       	  					k2=i;
        	  					rect2=rects.get(i).getRect();
        	  					rectangle = rects.get(i).getRect().getBounds2D();
        	  					rects.get(i).setP1(event.getX());
@@ -174,6 +176,7 @@ public class TimeLineView extends JPanel
 
 	   	  				rects.get(i).centerP();
 	   	  				rects.get(i).centerWidth();
+	   	  				rects.get(i).centerQ();
 //	   	  				System.out.println("s " + s + ", s2 " + s2 + " s3 " + s3);
 	   	  				if(s==1){
 	   	  					//rects.get(i).setWidth(s2);
@@ -182,6 +185,7 @@ public class TimeLineView extends JPanel
 	   	  					rects.get(i).setP(s2);
 	   	  				}
 		   	  			else{
+		   	  				interSector(k2);
 		   	  				if(s2!=0){
 		   	  					rects.get(i).setWidth(s2);
 		   	  				}
@@ -229,7 +233,7 @@ public class TimeLineView extends JPanel
 				    	    			}
 				    	    			else if(rects.size()>0 && i!=j && rects.get(i).getRect().intersects(rects.get(j).getRect())){
 				    	    				rects.get(j).addP(-15);
-				    	    				fixIntersect(j,-1);
+				    	    				fixIntersect(k2,-1);
 				    	    			}
 				    	    			else if(rects.size()==1){
 				    	    				rects.get(i).dragLeft(event.getX());
@@ -248,7 +252,7 @@ public class TimeLineView extends JPanel
 				    	    			}
 				    	    			else if(rects.size()>0 && i!=j && rects.get(i).getRect().intersects(rects.get(j).getRect())){
 				    	    				rects.get(j).addP(15);
-				    	    				fixIntersect(j,1);
+				    	    				fixIntersect(k2,1);
 				    	    			}
 				    	    			else if(rects.size()==1){
 				    	    				rects.get(i).dragRight(event.getX());
@@ -258,25 +262,25 @@ public class TimeLineView extends JPanel
 			    				
 			    			}
 			    			else{
-			    				for (int j =0; j<rects.size();j++){
-			        				if(rects.get(j)!=null){
+			    				//for (int j =0; j<rects.size();j++){
+//			        				if(rects.get(j)!=null){
 
-				    	    			if (i!=j && !rects.get(i).getRect().intersects(rects.get(j).getRect())){
-				    	    				rects.get(i).dragCenter(event.getX(), event.getY());	
-				    	    			}
-				    	    			else if(rects.size()>0 && i!=j && rects.get(i).getRect().intersects(rects.get(j).getRect())){
-				    	    				int k = rects.get(i).getAbs();
-				    	    				rects.get(j).addP(15*k);
+//				    	    			if (i!=j && !rects.get(i).getRect().intersects(rects.get(j).getRect())){
+//				    	    				rects.get(i).dragCenter(event.getX(), event.getY());	
+//				    	    			}
+				    	    			//else if(rects.size()>0 && i!=j && rects.get(i).getRect().intersects(rects.get(j).getRect())){
+				    	    				//int k = rects.get(i).getAbs();
+				    	    				//rects.get(j).addP(15*k);
 				    	    				//System.out.println(k); // höger blir negativt och vänster.
 				    	    				//System.out.println("1. ID " + rects.get(j).getID() + ", antal steg " + 15*k);
-				    	    				fixIntersect(j,k);
+				    	    				
 			
-				    	    			}
-				    	    			else if(rects.size()==1){
-				    	    				rects.get(i).dragCenter(event.getX(), event.getY());
-				    	    			}
-			        				}
-			    	    		}
+				    	    			//}
+//				    	    			else if(rects.size()==1){
+				    	    				rects.get(k2).dragCenter(event.getX(), event.getY());
+//				    	    			}
+//			        				}
+			    	    		//}
 			    				
 			    			}
 						}
@@ -288,10 +292,6 @@ public class TimeLineView extends JPanel
 	    	cursor = Cursor.getPredefinedCursor(Cursor.HAND_CURSOR);
 	    	x = event.getX();
 	    	y = event.getY();
-	    	
-	    	//System.out.println("mouse X = " + x);
-	    	//System.out.println("mouse Y = " + y);
-	    	
 	    }
     }
 
@@ -300,17 +300,70 @@ public class TimeLineView extends JPanel
 		Box temp=new Box(id*45, 5, 45, 20, id);
 		rects.add(temp);
 	}
-	public void fixIntersect(int id, int k) {
+//	public void fixIntersect(int id, int k) {
+//		for (int j =0; j<rects.size();j++){
+//			if(rects.get(j)!=null){
+//
+//				if(rects.size()>0 && id!=j && rects.get(id).getRect().intersects(rects.get(j).getRect())){
+//					rects.get(j).addP((15*k));
+//					//System.out.println("2. ID " + rects.get(j).getID() + ", antal steg " + 	15*k);
+//				}
+//			}
+//		}
+//	}
+	public void fixIntersect(int i, int k){
+		int numberOfIntersects=0;
+		int firstHit=-1;
 		for (int j =0; j<rects.size();j++){
-			if(rects.get(j)!=null){
-
-				if(rects.size()>0 && id!=j && rects.get(id).getRect().intersects(rects.get(j).getRect())){
-					rects.get(j).addP((15*k));
+			if(j!=i && rects.get(i).getRect()!=null && rects.get(j).getRect()!=null){
+				if(rects.get(i).getRect().intersects(rects.get(j).getRect())){
+					if(k>0){
+						rects.get(j).addP((int) (rects.get(i).getP()+rects.get(i).getWidth()-rects.get(j).getWidth()));
+						rects.get(j).addP(numberOfIntersects*5);
+						if(numberOfIntersects==0){
+							firstHit=j;
+						}
+					}
+					else{
+						rects.get(j).setP(rects.get(i).getP()-rects.get(j).getWidth());
+						rects.get(j).addP(numberOfIntersects*-5);
+						if(numberOfIntersects==0){
+							firstHit=j;
+						}
+					}
+					numberOfIntersects++;
 					//System.out.println("2. ID " + rects.get(j).getID() + ", antal steg " + 	15*k);
 				}
 			}
 		}
+		if(firstHit!=-1){
+			interSector(firstHit);
+			System.out.println("en rekursiv operation: " + numberOfIntersects);
+		}
 	}
+	public void interSector(int i){
+		int numberOfIntersects=0;
+		int firstHit=-1;
+		for (int j =0; j<rects.size();j++){
+			if(j!=i && rects.get(i).getRect()!=null && rects.get(j).getRect()!=null){
+				if(rects.get(i).getRect().intersects(rects.get(j).getRect())){
+					rects.get(j).addP((int) (rects.get(i).getP()+rects.get(i).getWidth()-rects.get(j).getP()));
+					rects.get(j).addP(numberOfIntersects*5);
+					if(numberOfIntersects==0){
+						firstHit=j;
+					}
+					numberOfIntersects++;
+					//System.out.println("2. ID " + rects.get(j).getID() + ", antal steg " + 	15*k);
+				}
+			}
+		}
+		if(firstHit!=-1){
+			interSector(firstHit);
+			System.out.println("en iterativ operation: " + numberOfIntersects);
+		}
+	}
+	
+	
 
 	public void dropTask(int selectedTask) {
 //		System.out.println("selectedTask: "  + selectedTask);
